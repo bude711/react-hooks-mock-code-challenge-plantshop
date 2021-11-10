0024c404a6ea6809e59502e2a54f0af4ffbfeedd
+ <brandi.ude@gmail.com>
2021-11-10 11:55:13 -0500

    I tried my best

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantList}) {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+         <PlantCard plantList={plantList}/>
+
+      </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({handleAddPlant}) {
+
+  const [search, setSearch] =useState("") 
+  const [plantList, setPlantList]=useState([])
+  const[plants, setPlants]=useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(data => {
+      setPlants(data)
+      setPlantList(data)
+    })
+  }, [])
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+    setPlantList(plants.filter(plantObj=>plantObj.name.includes(e.target.value)))
+  }
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleAddPlant={handleAddPlant}/>
+      <Search search={search} handleSearch={handleSearch}/>
+      <PlantList plantList={plantList}/>
     </main>
   );
 }
